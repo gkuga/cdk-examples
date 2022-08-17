22e4a74e121b5f8d76b340b61c26c7c9ae0630b9
gkuga <gaku.yasui@gmail.com>
2022-08-17 23:34:33 +0900

    Add hitcounter resouces

--- cdk_workshop/cdk_workshop/hitcounter.py
+++ cdk_workshop/cdk_workshop/hitcounter.py
@@ -1,10 +1,30 @@
+from aws_cdk import aws_dynamodb as ddb
 from aws_cdk import aws_lambda as _lambda
 from constructs import Construct
 
 
 class HitCounter(Construct):
 
-    def __init__(self, scope: Construct, id: str, downstream: _lambda.IFunction, **kwargs):
-        super().__init__(scope, id, **kwargs)
+    @property
+    def handler(self):
+        return self._handler
 
-        # TODO
+        def __init__(self, scope: Construct, id: str, downstream: _lambda.IFunction, **kwargs):
+            super().__init__(scope, id, **kwargs)
+
+            table = ddb.Table(
+                self, 'Hits',
+                partition_key={'name': 'path',
+                               'type': ddb.AttributeType.STRING}
+            )
+
+            self._handler = _lambda.Function(
+                self, 'HitCountHandler',
+                runtime=_lambda.Runtime.PYTHON_3_7,
+                handler='hitcount.handler',
+                code=_lambda.Code.from_asset('lambda'),
+                environment={
+                    'DOWNSTREAM_FUNCTION_NAME': downstream.function_name,
+                    'HITS_TABLE_NAME': table.table_name,
+                }
+            )
