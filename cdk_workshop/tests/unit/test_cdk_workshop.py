c5db54e73eabd2ab4229b6e5a11465ef5e28b6ed
gkuga <gaku.yasui@gmail.com>
2022-08-18 00:08:07 +0900

    Update test

--- cdk_workshop/tests/unit/test_cdk_workshop.py
+++ cdk_workshop/tests/unit/test_cdk_workshop.py
@@ -5,13 +5,25 @@ from aws_cdk import aws_lambda as _lambda
 from cdk_workshop.hitcounter import HitCounter
 
 
-def test_dynamodb_table_created():
+def test_lambda_has_env_vars():
     stack = Stack()
     HitCounter(stack, "HitCounter",
                downstream=_lambda.Function(stack, "TestFunction",
                                            runtime=_lambda.Runtime.PYTHON_3_7,
                                            handler='hello.handler',
-                                           code=_lambda.Code.from_asset('lambda')),
-               )
+                                           code=_lambda.Code.from_asset('lambda')))
+
     template = assertions.Template.from_stack(stack)
-    template.resource_count_is("AWS::DynamoDB::Table", 1)
+    envCapture = assertions.Capture()
+
+    template.has_resource_properties("AWS::Lambda::Function", {
+        "Handler": "hitcount.handler",
+        "Environment": envCapture,
+    })
+
+    assert envCapture.as_object() == {
+        "Variables": {
+            "DOWNSTREAM_FUNCTION_NAME": {"Ref": "TestFunction22AD90FC"},
+            "HITS_TABLE_NAME": {"Ref": "HitCounterHits079767E5"},
+        },
+    }
